d209eea74282d6cda39df3dc4b0f89b58c24aa4d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-10-30 17:20:58 +0100

    Add psci setup

--- purescript-pack.el
+++ purescript-pack.el
@@ -7,7 +7,9 @@
 ;; haskell-pack
 
 (require 'install-packages-pack)
-(install-packages-pack/install-packs '(purescript-mode))
+(install-packages-pack/install-packs '(purescript-mode
+                                       psci
+                                       repl-toggle))
 
 (require 'purescript-mode)
 
@@ -16,6 +18,12 @@
                                    '(capitalized-words-mode t)
                                    '(turn-on-eldoc-mode t)
                                    '(turn-on-purescript-indent t))))
+
+(require 'psci)
+(add-hook 'purescript-mode-hook 'inferior-psci-mode)
+
+(require 'repl-toggle)
+(add-to-list 'rtog/mode-repl-alist '(purescript-mode . psci))
 
 (provide 'purescript-pack)
 ;;; purescript-pack.el ends here
