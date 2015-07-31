a9eba89a2623320a8b0dea9edb83d3c97a6037b4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-07-31 09:48:36 +0200

    Refactor - use-package

--- purescript-pack.el
+++ purescript-pack.el
@@ -6,27 +6,20 @@
 
 ;; haskell-pack
 
-(require 'install-packages-pack)
-(install-packages-pack/install-packs '(purescript-mode
-                                       psci
-                                       repl-toggle))
+(use-package repl-toggle)
+(use-package psci)
+(use-package purescript-mode
+  :config
+  (add-hook 'purescript-mode-hook (lambda ()
+                                    (custom-set-variables
+                                     '(capitalized-words-mode t)
+                                     '(turn-on-eldoc-mode t))))
+  ;;(add-hook 'purescript-mode-hook 'turn-on-purescript-indentation)
+  (add-hook 'purescript-mode-hook 'turn-on-purescript-indent)
+  ;;(add-hook 'purescript-mode-hook 'turn-on-purescript-simple-indent)
+  (add-hook 'purescript-mode-hook 'inferior-psci-mode)
 
-(require 'purescript-mode)
-
-(add-hook 'purescript-mode-hook (lambda ()
-                                  (custom-set-variables
-                                   '(capitalized-words-mode t)
-                                   '(turn-on-eldoc-mode t))))
-
-;;(add-hook 'purescript-mode-hook 'turn-on-purescript-indentation)
-(add-hook 'purescript-mode-hook 'turn-on-purescript-indent)
-;;(add-hook 'purescript-mode-hook 'turn-on-purescript-simple-indent)
-
-(require 'psci)
-(add-hook 'purescript-mode-hook 'inferior-psci-mode)
-
-(require 'repl-toggle)
-(add-to-list 'rtog/mode-repl-alist '(purescript-mode . psci))
+  (add-to-list 'rtog/mode-repl-alist '(purescript-mode . psci)))
 
 (provide 'purescript-pack)
 ;;; purescript-pack.el ends here
