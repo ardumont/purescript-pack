6babfc9bbb6693a566631cfeaf29dd1aa9287e8e
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-11-06 02:32:10 +0100

    purescript-pack: Simplify pack

--- purescript-pack.el
+++ purescript-pack.el
@@ -6,17 +6,16 @@
 
 ;; haskell-pack
 
-(use-package psci)
-(use-package purescript-mode
-  :config
-  (add-hook 'purescript-mode-hook (lambda ()
-                                    (custom-set-variables
-                                     '(capitalized-words-mode t)
-                                     '(turn-on-eldoc-mode t))))
-  ;;(add-hook 'purescript-mode-hook 'turn-on-purescript-indentation)
-  (add-hook 'purescript-mode-hook 'turn-on-purescript-indent)
-  ;;(add-hook 'purescript-mode-hook 'turn-on-purescript-simple-indent)
-  (add-hook 'purescript-mode-hook 'inferior-psci-mode))
+(require 'psci)
+(require 'purescript-mode)
+(add-hook 'purescript-mode-hook (lambda ()
+				  (custom-set-variables
+				   '(capitalized-words-mode t)
+				   '(turn-on-eldoc-mode t))))
+;;(add-hook 'purescript-mode-hook 'turn-on-purescript-indentation)
+(add-hook 'purescript-mode-hook 'turn-on-purescript-indent)
+;;(add-hook 'purescript-mode-hook 'turn-on-purescript-simple-indent)
+(add-hook 'purescript-mode-hook 'inferior-psci-mode)
 
 (provide 'purescript-pack)
 ;;; purescript-pack.el ends here
